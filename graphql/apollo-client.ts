70face7d7b03b9c8b336f838d12c3b2dfd6d0558
Arthur Yan <arthuryan08@gmail.com>
2023-09-16 08:02:36 -0300

    Apollo client configs

--- graphql/apollo-client.ts
+++ graphql/apollo-client.ts
@@ -1,5 +1,8 @@
-import { ApolloClient } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_URL
-})
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
+    cache: new InMemoryCache()
+});
+
+export default client;
